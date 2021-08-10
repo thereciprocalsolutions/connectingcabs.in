716a75cb902e340346e903657122b653d405d09e
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-08-11 00:22:08 +0530

    Update gmap.js

--- js/gmap.js
+++ js/gmap.js
@@ -46,9 +46,9 @@ function calcRoute() {
             var d=result.routes[0].legs[0].distance.value/1000;
             n = d.toFixed(0);
             sedano=((n*12)+350)+" - "+((n*12)+500);
-            sedanr=2*((n*12)+350)+" - "+2*((n*12)+500);
-            suvo=((n*18)+350)+" - "+((n*16)+500);
-            suvr=2*((n*18)+350)+" - "+2*((n*16)+500);
+            sedanr=2*((n*11)+350)+" - "+2*((n*11)+500);
+            suvo=((n*18)+350)+" - "+((n*18)+500);
+            suvr=2*((n*14)+350)+" - "+2*((n*14)+500);
             traveller=(2*((n*22)+350))+" - "+(2*((n*22)+500));
            
           //  $("#output").html("<div class='result-table'> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />SUV Rate: ₹" +((n)*12)+"-"+((n)*15)+".<br />Sedan Rate: ₹" +((n)*8)+"-"+((n)*11)+".<br />Hatchback Rate: ₹" +((n)*16)+"-"+((n)*19)+ ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>");
